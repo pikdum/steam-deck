09c90de4a843006ffbcf5eee4590766d9bc4cffb
pikdum <pikdum@kuudere.moe>
2023-02-07 00:55:28 -0600

    feat: add update script

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ cd ~/.pikdum
 wget -O steam-deck.zip https://github.com/pikdum/steam-deck/archive/refs/heads/master.zip
 unzip -o steam-deck.zip
 rm steam-deck.zip
+
+ln -s ~/.pikdum/steam-deck-master/update.desktop ~/Desktop/pikdum-update.desktop
