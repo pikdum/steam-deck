ad972ba5f284b6ea446a573118864537945aa5c4
pikdum <pikdum@kuudere.moe>
2023-02-07 01:42:27 -0600

    feat: add skyrim-post-deploy.desktop

--- vortex/install-vortex.sh
+++ vortex/install-vortex.sh
@@ -19,3 +19,4 @@ ln -sf ~/.pikdum/steam-deck-master/vortex/pikdum-vortex.desktop ~/.local/share/a
 update-desktop-database || true
 
 rm -f ~/Desktop/install-vortex.desktop
+ln -sf ~/.pikdum/steam-deck-master/vortex/skyrim-post-deploy.desktop ~/Desktop/
