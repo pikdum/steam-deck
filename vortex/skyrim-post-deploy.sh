a64d9881fc538f31aaf4facad364ff0fc881b209
pikdum <pikdum@kuudere.moe>
2023-02-08 01:32:20 -0600

    feat: better support for skyrim on microsd

--- vortex/skyrim-post-deploy.sh
+++ vortex/skyrim-post-deploy.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-cd "$HOME/.steam/steam/steamapps/common/Skyrim Special Edition/"
+SKYRIM_INTERNAL="$HOME/.steam/steam/steamapps/common/Skyrim Special Edition/"
+SKYRIM_EXTERNAL="/run/media/mmcblk0p1/steamapps/common/Skyrim Special Edition/"
 
-if ! cmp --silent -- "skse64_loader.exe" "SkyrimSELauncher.exe"; then
-    echo "Swapping SkyrimSELauncher.exe for skse64_loader.exe"
-    cd "$HOME/.steam/steam/steamapps/common/Skyrim Special Edition/"
-    mv SkyrimSELauncher.exe _SkyrimSELauncher.exe
-    cp skse64_loader.exe SkyrimSELauncher.exe
-fi
+skse_setup() {
+    if [ -d "$1" ]; then
+        cd "$1"
+        if ! cmp --silent -- "skse64_loader.exe" "SkyrimSELauncher.exe"; then
+            echo "Swapping SkyrimSELauncher.exe for skse64_loader.exe"
+            mv SkyrimSELauncher.exe _SkyrimSELauncher.exe
+            cp skse64_loader.exe SkyrimSELauncher.exe
+        fi
+    fi
+}
+
+skse_setup "$SKYRIM_INTERNAL"
+skse_setup "$SKYRIM_EXTERNAL"
+
+APPDATA_VORTEX="$HOME/.config/steamtinkerlaunch/vortex/compatdata/pfx/drive_c/users/steamuser/AppData/Local/Skyrim Special Edition"
+APPDATA_INTERNAL="$HOME/.local/share/Steam/steamapps/compatdata/489830/pfx/drive_c/users/steamuser/AppData/Local/Skyrim Special Edition/"
+APPDATA_EXTERNAL="/run/media/mmcblk0p1/steamapps/compatdata/489830/pfx/drive_c/users/steamuser/AppData/Local/Skyrim Special Edition/"
 
 echo "Copying loadorder.txt and plugins.txt"
-cp "$HOME/.config/steamtinkerlaunch/vortex/compatdata/pfx/drive_c/users/steamuser/AppData/Local/Skyrim Special Edition"/* "$HOME/.local/share/Steam/steamapps/compatdata/489830/pfx/drive_c/users/steamuser/AppData/Local/Skyrim Special Edition/"
+cp "$APPDATA_VORTEX"/* "$APPDATA_INTERNAL" || true
+cp "$APPDATA_VORTEX"/* "$APPDATA_EXTERNAL" || true
 
 echo "Success! Exiting in 3..."
 sleep 3
