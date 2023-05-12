64e7e1c00de3e70ae0366580a70cb525c4b7dc8a
pikdum <pikdum@kuudere.moe>
2023-05-11 23:39:29 -0500

    fix: typo

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ wget -O steam-deck.zip https://github.com/pikdum/steam-deck/archive/refs/heads/m
 unzip -o steam-deck.zip
 rm steam-deck.zip
 
-./.pikdum/steam-deck-master/post-install.sh
+~/.pikdum/steam-deck-master/post-install.sh
 
 echo "Success! Exiting in 3..."
 sleep 3
