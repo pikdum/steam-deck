28f3e48661438c8513a443b4502c576188073d15
pikdum <pikdum@kuudere.moe>
2023-05-11 23:41:10 -0500

    fix: ignore ln -s failure

--- post-install.sh
+++ post-install.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 ln -sf ~/.pikdum/steam-deck-master/update.desktop ~/Desktop/pikdum-update.desktop
 
 if [ ! -f "$HOME/.local/share/applications/pikdum-vortex.desktop" ]; then
-    ln -s ~/.pikdum/steam-deck-master/vortex/install-vortex.desktop ~/Desktop/install-vortex.desktop
+    ln -s ~/.pikdum/steam-deck-master/vortex/install-vortex.desktop ~/Desktop/install-vortex.desktop || true
 fi
 
 mkdir -p /run/media/mmcblk0p1/vortex-downloads || true
