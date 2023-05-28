1f283bdab55d10bffa51cadd74d9bfc2104dfb68
pikdum <pikdum@kuudere.moe>
2023-05-28 18:23:23 -0500

    fix: don't re-add install vortex desktop icon

--- post-install.sh
+++ post-install.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 ln -sf ~/.pikdum/steam-deck-master/update.desktop ~/Desktop/pikdum-update.desktop
 
-if [ ! -f "$HOME/.local/share/applications/pikdum-vortex.desktop" ]; then
+if [ ! -f "$HOME/.local/share/applications/vortex.desktop" ]; then
     ln -s ~/.pikdum/steam-deck-master/vortex/install-vortex.desktop ~/Desktop/install-vortex.desktop || true
 fi
 
