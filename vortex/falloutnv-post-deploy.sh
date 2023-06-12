3004ad4420d8b898f5861c37b9e40c361d981f06
Bernardo Bandos <jbbandos@users.noreply.github.com>
2023-06-13 01:38:30 +0200

    feat: fallout new vegas support (#23)
    There is an (optional) pre-deploy file
    to link ini files with the vortex prefix

---
+++ vortex/falloutnv-post-deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+set -euxo pipefail
+
+FALLOUTNV_INTERNAL="$HOME/.steam/steam/steamapps/common/Fallout New Vegas/"
+FALLOUTNV_EXTERNAL="/run/media/mmcblk0p1/steamapps/common/Fallout New Vegas/"
+
+nvse_setup() {
+    if [ -d "$1" ] &&
+        [ -f "${1}f4se_loader.exe" ] &&
+        [ -f "${1}Fallout4Launcher.exe" ]; then
+        cd "$1"
+        if ! cmp --silent -- "nvse_loader.exe" "FalloutNVLauncher.exe"; then
+            echo "Swapping FalloutNVLauncher.exe for nvse_loader.exe"
+            mv FalloutNVLauncher.exe _FalloutNVLauncher.exe
+            cp nvse_loader.exe FalloutNVLauncher.exe
+        fi
+    fi
+}
+
+nvse_setup "$FALLOUTNV_INTERNAL"
+nvse_setup "$FALLOUTNV_EXTERNAL"
+
+APPDATA_VORTEX="$HOME/.vortex-linux/compatdata/pfx/drive_c/users/steamuser/AppData/Local/falloutnv/"
+APPDATA_INTERNAL="$HOME/.local/share/Steam/steamapps/compatdata/22380/pfx/drive_c/users/steamuser/AppData/Local/falloutnv/"
+APPDATA_EXTERNAL="/run/media/mmcblk0p1/steamapps/compatdata/22380/pfx/drive_c/users/steamuser/AppData/Local/falloutnv/"
+
+echo "Copying loadorder.txt and plugins.txt"
+mkdir -p "$APPDATA_INTERNAL" || true
+mkdir -p "$APPDATA_EXTERNAL" || true
+cp "$APPDATA_VORTEX"/* "$APPDATA_INTERNAL" || true
+cp "$APPDATA_VORTEX"/* "$APPDATA_EXTERNAL" || true
+
+echo "Success! Exiting in 3..."
+sleep 3
