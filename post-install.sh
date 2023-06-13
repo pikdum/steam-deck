80f22262c509690f192694f6abf657fd2be73cc1
Joshua <twogoodgamer2@gmail.com>
2023-06-13 16:14:16 -0600

    feat: oblivion support (#24)
    * Add Oblivion Support
    
    Added Oblivion Support:
    
    - Created a oblivion-post-deploy.sh script
    - Created a oblivion-post-deploy.desktop script
    
    - Modified install-vortex.sh script to copy oblivion-post-deploy.desktop to steam deck desktop.
    
    * Add Post Deploy .desktops when updated
    
    Figured the most simple way to add any post-deploys a person does not have on there desktop is to simply  just copy and paste them there. It should replace any that are existing automatically so there should be no duplicates.
    
    * Update post-install.sh
    
    * Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -6,6 +6,11 @@ ln -sf ~/.pikdum/steam-deck-master/update.desktop ~/Desktop/pikdum-update.deskto
 
 if [ ! -f "$HOME/.local/share/applications/vortex.desktop" ]; then
     ln -s ~/.pikdum/steam-deck-master/vortex/install-vortex.desktop ~/Desktop/install-vortex.desktop || true
+else 
+    ln -sf ~/.pikdum/steam-deck-master/vortex/skyrim-post-deploy.desktop ~/Desktop/
+    ln -sf ~/.pikdum/steam-deck-master/vortex/fallout4-post-deploy.desktop ~/Desktop/
+    ln -sf ~/.pikdum/steam-deck-master/vortex/falloutnv-post-deploy.desktop ~/Desktop/
+    ln -sf ~/.pikdum/steam-deck-master/vortex/oblivion-post-deploy.desktop ~/Desktop/
 fi
 
 mkdir -p /run/media/mmcblk0p1/vortex-downloads || true
