0cebb82c74a0c055f70a59f14b7f0d7eedc1d9ca
Joshua <twogoodgamer2@gmail.com>
2023-06-13 13:35:26 -0600

    Add Post Deploy .desktops when updated
    Figured the most simple way to add any post-deploys a person does not have on there desktop is to simply  just copy and paste them there. It should replace any that are existing automatically so there should be no duplicates.

--- post-install.sh
+++ post-install.sh
@@ -8,4 +8,10 @@ if [ ! -f "$HOME/.local/share/applications/vortex.desktop" ]; then
     ln -s ~/.pikdum/steam-deck-master/vortex/install-vortex.desktop ~/Desktop/install-vortex.desktop || true
 fi
 
+ln -sf ~/.local/share/applications/vortex.desktop ~/Desktop/
+ln -sf ~/.pikdum/steam-deck-master/vortex/skyrim-post-deploy.desktop ~/Desktop/
+ln -sf ~/.pikdum/steam-deck-master/vortex/fallout4-post-deploy.desktop ~/Desktop/
+ln -sf ~/.pikdum/steam-deck-master/vortex/falloutnv-post-deploy.desktop ~/Desktop/
+ln -sf ~/.pikdum/steam-deck-master/vortex/oblivion-post-deploy.desktop ~/Desktop/
+
 mkdir -p /run/media/mmcblk0p1/vortex-downloads || true
