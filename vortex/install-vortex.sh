062061fb2939e6fbe5774055291b2ed43b02de0d
pikdum <pikdum@kuudere.moe>
2023-08-22 02:10:50 -0500

    chore: update vortex-linux

--- vortex/install-vortex.sh
+++ vortex/install-vortex.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-VORTEX_LINUX="v1.3.1"
+VORTEX_LINUX="v1.3.2"
 PROTON_URL="https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton8-13/GE-Proton8-13.tar.gz"
 VORTEX_URL="https://github.com/Nexus-Mods/Vortex/releases/download/v1.9.3/vortex-setup-1.9.3.exe"
 DOTNET_URL="https://download.visualstudio.microsoft.com/download/pr/06239090-ba0c-46e2-ad3e-6491b877f481/c5e4ab5e344eb3bdc3630e7b5bc29cd7/windowsdesktop-runtime-6.0.21-win-x64.exe"
